76f5d540944762698056f2c79bf42b1d74e2dd25
Link <link19970507@gmail.com>
2019-12-23 11:24:56 +0800

    fix: day-1

--- day1-num.js
+++ day1-num.js
@@ -4,7 +4,41 @@
 
 const twoSum = function (num, target) {
 
+  for (var i = 0; i < num.length; i++) {
+    for (var j = num.length; j > 0; j--) {
+      if (num[i] + num[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+
+  return []
+
 }
 
 
-console.log(twoSum([1,3,45,6,23,4], 5).join('-') === '0-5')
+console.log(twoSum([1,3,45,6,23,4], 4).join('-') === '0-1')
+console.log(twoSum([1,3,45,6,23,4], 29).join('-') === '3-4')
+
+/**
+ * 优化 两数之和
+ * @param num
+ * @param target
+ */
+const optimization = function (num, target) {
+  const map = new Map(num.map((item, index) => {
+    return [item, index]
+  }))
+
+  for (var i = 0, len = num.length; i < len; i++) {
+    const last = target - num[i]
+    if (map.has(last)) {
+      return [i, map.get(last)]
+    }
+  }
+
+  return []
+}
+
+console.log(optimization([1,3,45,6,23,4], 4).join('-') === '0-1')
+console.log(optimization([1,3,45,6,23,4], 29).join('-') === '3-4')
