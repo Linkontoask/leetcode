9fad32780b2c2132a889def1f4073c7815fbc843
Link <link19970507@gmail.com>
2019-12-24 14:11:35 +0800

    fix: day-2 回文 未解决

---
+++ day2-palindrome.js
@@ -0,0 +1,73 @@
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+ 示例 1：
+
+ 输入: "babad"
+ 输出: "bab"
+ 注意: "aba" 也是一个有效答案。
+ 示例 2：
+
+ 输入: "cbbd"
+ 输出: "bb"
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * 找出相同字符最长子串
+ * @param {string} s
+ * @return {number}
+ */
+var longSame = function(s) {
+
+  const array = [...s]
+
+  let max = []
+
+  if (array.length === 0) {
+    return 0
+  }
+  if (array.length === 1) {
+    return 1
+  }
+  for (var i = 0, len = array.length; i < len; i++) {
+    const item = array[i]
+    for (var j = len - 1; j > i; j--) {
+      const last = array[j]
+      if (item === last) {
+        let count = j - i + 1
+        max.push([].concat(array).splice(i, count))
+      }
+    }
+  }
+
+  return Math.max(...max.map(item => item.length))
+};
+
+console.log(longSame("aaaaa"))
+
+/**
+ * 回文 未解决
+ * @param {string} s
+ * @return {string}
+ */
+function longestPalindrome (s) {
+
+  let arrayReverse = [...s].reverse()
+
+  while(arrayReverse.length) {
+    if (s.includes(arrayReverse.join(''))) {
+      console.log(arrayReverse)
+      break;
+    } else {
+      arrayReverse.shift()
+    }
+  }
+
+  return arrayReverse.reverse().join('')
+}
+
+console.log(longestPalindrome("abacdfgdcaba"))
